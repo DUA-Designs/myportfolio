3309237966848d59b209e3dec17223a1c71cfb82
Uday Aravind <140690275+DUA-Designs@users.noreply.github.com>
2023-11-29 19:48:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,31 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+ 
+const url =  "https://api.adviceslip.com/advice";
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6fbfebbdc6msh5f6f5ee3beae5d2p1fc4d9jsn61a0a7629cf5',
+		'X-RapidAPI-Host': 'shakespeare1.p.rapidapi.com'
+	}
+};
+
+ 
+ 
+
+try {
+	const response = fetch(url );
+	 response.then((result)=> result.json()).then((data)=>{
+    console.log(data);
+     document.getElementById("quote").innerHTML=data.slip.advice;
+   document.getElementById("author").innerHTML=`Advice- #${data.slip.id}`});
+} catch (error) {
+	console.error(error);
+}
+
+ /*
+  document.getElementById("quote").innerHTML=data[0].text;
+  document.getElementById("author").innerHTML=`Author- ${data[0].author}`
+ });  */
