86f4889198bd079b0601dbb6d7271b5e45f3e142
Uday Aravind <140690275+DUA-Designs@users.noreply.github.com>
2023-12-03 20:13:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,31 @@ if(date.getHours()===23 && date.getSeconds()===10 && date.getMinutes()===59){
 
 }
 
+function sendEmail(e){
+  e.preventDefault();
+
+  let inputName3=document.getElementById("inputName3").value;
+  let inputEmail3=document.getElementById("inputEmail3").value;
+  let message=document.getElementById("inputMessage").value;
+  var templateParams = {
+    from_name:  inputName3,
+    emial_id:inputEmail3,
+    to_name:"Aravind",
+    message
+ 
+
+};
+ 
+ 
+  emailjs.init("I4hRM78EXk7LdR9pC");
+  emailjs.send('service_i12y271', 'template_l0cryvm',templateParams )
+    .then(function(response) {
+       alert('Your response has been recorded!', response.status, response.text);
+    }, function(error) {
+       alert('FAILED...', error);
+    });
+}
+
  
  
 
